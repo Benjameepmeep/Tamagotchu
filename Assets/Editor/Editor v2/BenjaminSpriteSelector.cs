79cf03a8a5f7aad7a95a582a089e1dcdf56e1747
Benjamin Alexander <146328131+Benjameepmeep@users.noreply.github.com>
2024-09-15 13:23:39 +0200

    mini-changes to my script

--- Assets/Editor/Editor v2/BenjaminSpriteSelector.cs
+++ Assets/Editor/Editor v2/BenjaminSpriteSelector.cs
@@ -12,10 +12,12 @@ public class BenjaminSpriteSelector : EditorWindow
         wnd.titleContent = new GUIContent("BenjaminSpriteSelector");
     }
 
+    private VisualElement _rightPane;
+
     public void CreateGUI()
     {
         // Get a list of all sprites in the project
-        string [] spriteObjectGuids = AssetDatabase.FindAssets("t:Sprite", new[] { "Assets/Sprites" });
+        var spriteObjectGuids = AssetDatabase.FindAssets("t:Sprite", new[] { "Assets" });
         var allSpriteObjects = new List<Sprite>();
         foreach (var guid in spriteObjectGuids)
         {
@@ -30,13 +32,40 @@ public class BenjaminSpriteSelector : EditorWindow
 
         // A TwoPaneSplitView needs exactly two child elements.
         var leftPane = new ListView();
-        splitView.Add(leftPane); 
-        var rightPane = new VisualElement();
-        splitView.Add(rightPane);
+        splitView.Add(leftPane);
+        _rightPane = new VisualElement();
+        splitView.Add(_rightPane);
 
         // Initialize the list view with all sprites' names
         leftPane.makeItem = () => new Label();
         leftPane.bindItem = (item, index) => { (item as Label).text = allSpriteObjects[index].name; };
         leftPane.itemsSource = allSpriteObjects;
+
+        // React to the user's selection
+        leftPane.selectionChanged += OnSpriteSelectionChange;
+    }
+
+    private void OnSpriteSelectionChange(IEnumerable<object> selectedItems)
+    {
+        // Clear all previous content from the pane.
+        _rightPane.Clear();
+
+        // Get the selected sprite and display it.
+        var enumerator = selectedItems.GetEnumerator();
+        if (enumerator.MoveNext())
+        {
+            var selectedSprite = enumerator.Current as Sprite;
+            if (selectedSprite != null)
+            {
+                // Add a new Image control and display the sprite.
+                var spriteImage = new Image();
+                spriteImage.scaleMode = ScaleMode.ScaleToFit;
+                spriteImage.sprite = selectedSprite;
+
+                // Add the Image control to the right-hand pane.
+                _rightPane.Add(spriteImage);
+            }
+
+        }
     }
 }
